08ec79b76c50b1731bf6b6b343d83400efc28ecf
Einhornhool <lena.boeckmann@moia.io>
2024-03-21 12:56:18 +0000

    New structure

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -eu
-$CXX $CXXFLAGS $SRC/fuzz_libfuzzer.cpp $SRC/bad_code.cpp -o $OUT/fuzz_badcode $LIB_FUZZING_ENGINE
+$CXX $CXXFLAGS -I$SRC/src/ $SRC/test_libfuzzer/fuzz_libfuzzer.cpp $SRC/src/bad_code.cpp -o $OUT/fuzz_badcode $LIB_FUZZING_ENGINE
